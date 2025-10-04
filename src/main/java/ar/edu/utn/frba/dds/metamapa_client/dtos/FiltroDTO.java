b25cec52dcaf875d9c280d5f7802d0035bd58046
mgorino <mgorino@frba.utn.edu.ar>
2025-10-04 15:42:19 -0300

    Hice que funcionara el flujo de nav-hechos por coleccion y por el nav-hechos con todos los hechos. Solo falta arreglar el filtro de categoria del nav-hechos y hacer que funcione el de texto

--- src/main/java/ar/edu/utn/frba/dds/metamapa_client/dtos/FiltroDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/metamapa_client/dtos/FiltroDTO.java
@@ -5,13 +5,18 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 public class FiltroDTO {
   String categoria;
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
   LocalDateTime fecha_reporte_desde;
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
   LocalDateTime fecha_reporte_hasta;
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
   LocalDateTime fecha_acontecimiento_desde;
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
   LocalDateTime fecha_acontecimiento_hasta;
   Float latitud;
   Float longitud;
